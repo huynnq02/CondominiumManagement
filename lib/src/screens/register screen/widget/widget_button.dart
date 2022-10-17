bcaf575913e96a0da4fdb6824e29f099a4d18ce0
haileyng02 <haileynguyenss@gmail.com>
2022-10-18 01:26:56 +0700

    edit button

--- lib/src/screens/register screen/widget/widget_button.dart
+++ lib/src/screens/register screen/widget/widget_button.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+
 class WidgetButton extends StatelessWidget {
   final String labelText;
   final void Function()? onPressed;
-  const WidgetButton({
-    Key? key,
-    required this.labelText,
-    this.onPressed
-  }) : super(key: key);
+  const WidgetButton({Key? key, required this.labelText, this.onPressed})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(12),
-            boxShadow: [
-              BoxShadow(
-                  color: Colors.black.withOpacity(0.25),
-                  offset: const Offset(0, 4),
-                  blurRadius: 4)
-            ]),
+        decoration:
+            BoxDecoration(borderRadius: BorderRadius.circular(12), boxShadow: [
+          BoxShadow(
+              color: Colors.black.withOpacity(0.25),
+              offset: const Offset(0, 4),
+              blurRadius: 4)
+        ]),
         child: MaterialButton(
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12),
@@ -28,11 +24,10 @@ class WidgetButton extends StatelessWidget {
             color: Colors.white,
             onPressed: onPressed,
             child: Container(
-              padding: const EdgeInsets.symmetric(
-                  horizontal: 30, vertical: 14),
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 14),
               child: Text(
                 labelText,
-                style: GoogleFonts.lexendExa(fontSize: 16),
+                style: TextStyle(fontSize: 16),
               ),
             )),
       ),
