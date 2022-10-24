1ca6acd9d93abce2f34799aab4a3f389aba4fe3f
huyy802 <20521401@gm.uit.edu.vn>
2022-10-24 21:23:04 +0700

    chore: fix conflict

--- lib/src/screens/profile screen/profile_screen.dart
+++ lib/src/screens/profile screen/profile_screen.dart
@@ -1,7 +1,4 @@
 import "package:flutter/material.dart";
-import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:untitled/src/screens/profile%20screen/widget/profile_picture.dart';
-import 'package:untitled/utils/app_constant/app_text_style.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -11,22 +8,10 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
-  String userName = "Huy Nguyen";
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height;
-    final width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: Container(
-        height: height,
-        width: width,
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/login-screen-background.png"),
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
+    return Container(
+      child: Text("OK nhe"),
     );
   }
 }
