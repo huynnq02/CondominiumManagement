9a936c625ce049b6cba78a52e5730ea2238a9219
huyy802 <20521401@gm.uit.edu.vn>
2022-10-24 22:30:39 +0700

    chore(profile screen): Add screen background

--- lib/src/screens/profile screen/profile_screen.dart
+++ lib/src/screens/profile screen/profile_screen.dart
@@ -1,4 +1,6 @@
 import "package:flutter/material.dart";
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'package:untitled/utils/app_constant/app_text_style.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -10,8 +12,19 @@ class ProfileScreen extends StatefulWidget {
 class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("OK nhe"),
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Container(
+        height: height,
+        width: width,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/login-screen-background.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
     );
   }
 }
