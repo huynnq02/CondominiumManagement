b63eaa27d432c5090ee80342bfaced61f2852c84
Nam Hung <17520544@ms.uit.edu.vn>
2022-11-13 22:40:32 +0700

    Task 410- update feedbackAPI_Provider

--- lib/repository/feedback/feedbackAPI_Provider.dart
+++ lib/repository/feedback/feedbackAPI_Provider.dart
@@ -1,47 +1,26 @@
-import 'package:dio/dio.dart';
-import 'package:untitled/repository/base/base_repository.dart';
+import 'dart:convert';
+
+import 'package:untitled/repository/base/base_provider.dart';
+import 'package:untitled/repository/feedback/feedbackAPI_repository.dart';
 import 'package:untitled/src/models/feedback.dart';
 
-class FeedbackAPIRepository extends BaseRepository {
-  Future<Response> createFeedbackAPIRepository(Feedback feedback) async {
-    try {
-      // print(feedback.toJson());
-      var client = init();
-      print("zo ne");
-      print(feedback.toJson());
-      final feedbackResponse = await client.post(
-        '/api/UserFeedback',
-        data: {
-          "id": 0,
-          "email": feedback.email,
-          "type": feedback.type,
-          "time": feedback.time,
-          "title": feedback.title,
-          "status": feedback.status,
-          "content": feedback.content,
-          "respond": feedback.respond,
-          "image": feedback.image,
-        },
-      );
-      print("ez qua");
-      return feedbackResponse;
-    } on DioError catch (error) {
-      return error.response as Response;
-    }
+class FeedbackAPIProvider extends BaseProvider<FeedbackAPIRepository> {
+  @override
+  FeedbackAPIRepository initRepository() {
+    return FeedbackAPIRepository();
   }
 
-  Future<Response> getUserFeedbackAPIRepository() async {
-    try {
-      // print(feedback.toJson());
-      var client = init();
-      print("zo ne");
-      final feedbackResponse = await client.get(
-        '/api/UserFeedback',
-      );
-      print("ez qua");
-      return feedbackResponse;
-    } on DioError catch (error) {
-      return error.response as Response;
-    }
+  Future createFeedbackAPIProvider(Feedback feedback) async {
+    var res = await repository.createFeedbackAPIRepository(feedback);
+    print("ret data");
+    print(res.data['success']);
+    return res.data['success'];
+  }
+
+  Future getUserFeedbackAPIProvider() async {
+    var res = await repository.getUserFeedbackAPIRepository();
+    print("ret data");
+    print(res.data);
+    return res.data;
   }
 }
