6586d00a314550394c7b219531c9b78dda982ffd
haileyng02 <haileynguyenss@gmail.com>
2022-12-22 03:32:11 +0700

    added register with phone number feature

--- lib/src/screens/login screen/login_screen.dart
+++ lib/src/screens/login screen/login_screen.dart
@@ -33,107 +33,111 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: AppColors.White,
-      body: GestureDetector(
-        onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
-        child: Form(
-          key: _formKey,
-          child: SafeArea(
-            bottom: false,
-            child: Padding(
-              padding:
-                  EdgeInsets.symmetric(horizontal: 35, vertical: height * 0.03),
-              child: Column(
-                children: [
-                  const Text(
-                    'Đăng nhập',
-                    style: TextStyle(
-                        color: Color(0xFF58583A),
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40),
-                  ),
-                  SizedBox(
-                    height: height * 0.036,
-                  ),
-                  Image.asset(
-                    'assets/apato-logo-light-blue.png',
-                    width: width * 0.34,
-                  ),
-                  CustomTextField(
-                    controller: emailController,
-                    labelText: 'Email/ Số điện thoại',
-                    type: TextFieldType.email,
-                    maxLength: 50,
-                  ),
-                  SizedBox(
-                    height: height * 0.03,
-                  ),
-                  CustomTextField(
-                    controller: passwordController,
-                    labelText: 'Mật khẩu',
-                    type: TextFieldType.password,
-                                      maxLength: 20,
-                  ),
-                  SizedBox(
-                    height: height * 0.03,
-                  ),
-                  CustomButton(
-                    label: 'Đăng nhập',
-                    onPressed: () {
-                      setState(() {});
-                      if (_formKey.currentState!.validate()) {
-                        AppPreferences.prefs.setBool('save',
-                            checkSave); //lưu trạng thái của nút lưu thông tin
-
-                        //Hiển thị trạng thái loading
-                        showDialog(
-                            context: context,
-                            builder: (context) {
-                              return Center(
-                                child: CircularProgressIndicator(),
-                              );
-                            });
-
-                        loginProvider!.checkAccount(
-                            // kiểm tra đăng nhập
-                            emailController.text,
-                            passwordController.text,
-                            checkSave,
-                            context);
-                      }
-                    },
-                  ),
-                  SizedBox(height: height * 0.03),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: ((context) => ForgetPasswordScreen())));
-                    },
-                    child: Text(
-                      "Quên mật khẩu ?",
-                      style: TextStyle(color: Colors.black, fontSize: 16),
+    return WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: AppColors.White,
+        body: GestureDetector(
+          onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+          child: Form(
+            key: _formKey,
+            child: SafeArea(
+              bottom: false,
+              child: Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: 35, vertical: height * 0.03),
+                child: Column(
+                  children: [
+                    const Text(
+                      'Đăng nhập',
+                      style: TextStyle(
+                          color: Color(0xFF58583A),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 40),
                     ),
-                  ),
-                  Spacer(),
-                  Text(
-                    'Chưa có tài khoản? ',
-                    style: TextStyle(fontWeight: FontWeight.w300, fontSize: 16),
-                  ),
-                  SizedBox(
-                    height: height * 0.02,
-                  ),
-                  CustomButton(
+                    SizedBox(
+                      height: height * 0.036,
+                    ),
+                    Image.asset(
+                      'assets/apato-logo-light-blue.png',
+                      width: width * 0.34,
+                    ),
+                    CustomTextField(
+                      controller: emailController,
+                      labelText: 'Email/ Số điện thoại',
+                      type: TextFieldType.email,
+                      maxLength: 50,
+                    ),
+                    SizedBox(
+                      height: height * 0.03,
+                    ),
+                    CustomTextField(
+                      controller: passwordController,
+                      labelText: 'Mật khẩu',
+                      type: TextFieldType.password,
+                      maxLength: 20,
+                    ),
+                    SizedBox(
+                      height: height * 0.03,
+                    ),
+                    CustomButton(
+                      label: 'Đăng nhập',
                       onPressed: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: ((context) => RegisterScreen())));
+                        setState(() {});
+                        if (_formKey.currentState!.validate()) {
+                          AppPreferences.prefs.setBool('save',
+                              checkSave); //lưu trạng thái của nút lưu thông tin
+
+                          //Hiển thị trạng thái loading
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return Center(
+                                  child: CircularProgressIndicator(),
+                                );
+                              });
+
+                          loginProvider!.checkAccount(
+                              // kiểm tra đăng nhập
+                              emailController.text,
+                              passwordController.text,
+                              checkSave,
+                              context);
+                        }
                       },
-                      label: 'Đăng ký'),
-                  SizedBox(
-                    height: height * 0.02,
-                  )
-                ],
+                    ),
+                    SizedBox(height: height * 0.03),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: ((context) => ForgetPasswordScreen())));
+                      },
+                      child: Text(
+                        "Quên mật khẩu ?",
+                        style: TextStyle(color: Colors.black, fontSize: 16),
+                      ),
+                    ),
+                    Spacer(),
+                    Text(
+                      'Chưa có tài khoản? ',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w300, fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: height * 0.02,
+                    ),
+                    CustomButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: ((context) => RegisterScreen())));
+                        },
+                        label: 'Đăng ký'),
+                    SizedBox(
+                      height: height * 0.02,
+                    )
+                  ],
+                ),
               ),
             ),
           ),
